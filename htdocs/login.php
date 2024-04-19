16447562065214d349a7c1206dbc85580bad70d7
Pedro Benegas <pedrophbs8199@gmail.com>
2024-04-19 19:05:59 -0300

    github actions teste

---
+++ htdocs/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+    <title>Blog Project</title>
+</head>
+<body>
+    <div id="background">
+
+        <div id="container">
+            <div id="formContainer">
+                <form action="" method="">
+                <div id="inputs">
+                    <div id="loginTitle">
+                        <p id="loginText">Blog Project!</p>
+                    </div>
+                    <div>
+                        <input type="text" class="loginFields" name="user" id="user">
+                    </div>
+                    <div>
+                        <input type="password" class="loginFields" name="password" id="password">
+                    </div>
+                    <div id="btnField">
+                        <button class="loginBtn" role="button">Login</button>
+                    </div>
+
+                    <div id="registerLink">
+                        <a href="#">Click here to create an account!</a>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
