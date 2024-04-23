e4ad3ef3dc7eb1f3b3efc2b4bb2aa74fdd21e377
Pedro Benegas <pedrophbs8199@gmail.com>
2024-04-22 23:19:25 -0300

    fix #1 register screen is now responsive

--- htdocs/BlogProject/register.php
+++ htdocs/BlogProject/register.php
@@ -9,18 +9,16 @@
 <body>
     <div id="background">
 
-        <div id="container">
+        <div>
             <div id="registerFormContainer">
                 <form action="" method="">
-                <div id="inputs">
                     <div id="loginTitle">
-                        <p id="loginText">Sign up to start bloging!</p>
+                        <p id="registerText">Sign up to start bloging!</p>
                     </div>
-                    <div class="container">
+
+                    <div id="inputs">
                         <input type="text" name="user" id="user" class="formFields" placeholder="USER">
                         <input type="email" name="email" id="email" class="formFields" placeholder="E-MAIL">
-                    </div>
-                    <div class="container">
                         <input type="password" name="password" id="password" class="formFields" placeholder="PASSWORD">
                         <input type="password" name="confirmPassword" id="confirmPassword" class="formFields" placeholder="CONFIRM PASSWORD">
                     </div>
@@ -31,9 +29,9 @@
                     <div id="registerLink">
                         <a href="index.php">Alredy signed up? click here to login!</a>
                     </div>
-                </div>
                 </form>
             </div>
+            
         </div>
 
     </div>
