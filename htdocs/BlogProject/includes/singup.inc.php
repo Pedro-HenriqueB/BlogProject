dd423896c18f7c193aee6006905efd5cc35cb129
Pedro Benegas <pedrophbs8199@gmail.com>
2024-04-30 20:47:51 -0300

    update: starting the signup controller

---
+++ htdocs/BlogProject/includes/singup.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST["submit"]))
+{
+    // Grabbing data
+    $uid = $_POST["user"];
+    $email = $_POST["email"];
+    $pwd = $_POST["password"];
+    $pwdrepeat = $_POST["confirmPassword"];
+
+    // Istantiate SignupContr class
+
+    // Runing error handlers and user signup
+
+    // Going back to front page
+}
