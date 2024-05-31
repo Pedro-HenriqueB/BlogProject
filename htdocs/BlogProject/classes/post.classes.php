e4764eb2bc88ae40ee33f6275160d9de6a9879cc
Pedro Benegas <pedrophbs8199@gmail.com>
2024-05-31 20:06:52 -0300

    update: menu layout finished, creating post features

---
+++ htdocs/BlogProject/classes/post.classes.php
@@ -0,0 +1,12 @@
+<?php
+
+class Post extends Dbh {
+    protected function CreatePost($uid, $title, $post){
+        $stmt = $this->connect()->prepare('INSERT INTO posts (user_id, title, post) VALUES (?, ?, ?)');
+        if($stmt->execute($uid, $title, $post)){
+            $stmt = null;
+            header('Location: ../post.php?error=stmtfailed');
+            exit();
+        }
+    }
+}
